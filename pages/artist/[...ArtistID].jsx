cac8397e76b6f8a53c0d5196cd020205995105bc
Janzie21 <nick_janssen21@hotmail.com>
2021-06-21 16:09:47 +0200

    table names changed

--- pages/artist/[...ArtistID].jsx
+++ pages/artist/[...ArtistID].jsx
@@ -51,7 +51,7 @@ export default ArtistDetail;
 
 export async function getServerSideProps(context) {
   const sql =
-    'SELECT artists.*, JSON_ARRAYAGG(JSON_OBJECT("url", `social-links`.`Url`, "id", `social-links`.id)) AS links, JSON_ARRAYAGG( JSON_OBJECT("id", services.ServicesID, "naam", `services`.`Naam`)) AS services FROM artists JOIN `social-links` ON artists.ArtistID = `social-links`.Artists_ArtistID JOIN `services` ON services.ServicesID = `social-links`.`Services_ServicesID` GROUP BY ArtistID';
+    'SELECT artists.*, JSON_ARRAYAGG(JSON_OBJECT("url", `social-links`.`Url`, "id", `social-links`.id)) AS links, JSON_ARRAYAGG(JSON_OBJECT("id", services.ServicesID, "naam", `services`.`Naam`)) AS services FROM artists JOIN `social-links` ON artists.ArtistID = `social-links`.Artists_ArtistID JOIN `services` ON services.ServicesID = `social-links`.`Services_ServicesID` GROUP BY ArtistID';
   const res = await knex.raw(sql);
   const [id] = context.query.ArtistID;
   console.log(id);
